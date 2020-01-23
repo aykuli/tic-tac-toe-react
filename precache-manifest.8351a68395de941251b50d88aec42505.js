0a307ddd73e72e723be4ddb456df5454f3c02c01
Aykuli <aykuli@ya.ru>
2020-01-23 14:10:00 +0700

    feat: add builded files

---
+++ precache-manifest.8351a68395de941251b50d88aec42505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3db4859c5822fa84f027dcec745248e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02fb277fb784b14166a7",
+    "url": "/static/css/main.abf3768b.chunk.css"
+  },
+  {
+    "revision": "af478b6ac43632db1334",
+    "url": "/static/js/2.dd77b70f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.dd77b70f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02fb277fb784b14166a7",
+    "url": "/static/js/main.17f82b12.chunk.js"
+  },
+  {
+    "revision": "76a449627c6582e0e933",
+    "url": "/static/js/runtime-main.c31dca3c.js"
+  }
+]);
